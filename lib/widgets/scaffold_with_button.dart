52fd475d53443d538fdd3f032f4419615a571063
Tamim Arafat <31634638+arafatamim@users.noreply.github.com>
2021-09-12 05:10:12 +0600

    remove shortcut from scaffold_with_button

--- lib/widgets/scaffold_with_button.dart
+++ lib/widgets/scaffold_with_button.dart
@@ -1,41 +1,39 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:goribernetflix/utils.dart';
 
 class ScaffoldWithButton extends StatelessWidget {
   final Widget child;
   final AppBar? appBar;
 
-  const ScaffoldWithButton({required this.child, this.appBar, Key? key}) : super(key: key);
+  const ScaffoldWithButton({
+    required this.child,
+    this.appBar,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Shortcuts(
-      shortcuts: {
-        LogicalKeySet(LogicalKeyboardKey.select): const ActivateIntent()
-      },
-      child: Scaffold(
-        floatingActionButton: coalesceException(
-          () => Platform.isLinux
-              ? FloatingActionButton(
-                  onPressed: () => Navigator.of(context).pop(),
-                  child: const Icon(Icons.arrow_back),
-                )
-              : null,
-          null,
+    return Scaffold(
+      floatingActionButton: coalesceException(
+        () => Platform.isLinux
+            ? FloatingActionButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Icon(Icons.arrow_back),
+              )
+            : null,
+        null,
+      ),
+      key: key,
+      appBar: appBar,
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniStartFloat,
+      body: ProgressIndicatorTheme(
+        data: ProgressIndicatorThemeData(
+          color: Theme.of(context).colorScheme.secondary,
         ),
-        key: key,
-        appBar: appBar,
-        floatingActionButtonLocation:
-            FloatingActionButtonLocation.miniStartFloat,
-        body: ProgressIndicatorTheme(
-          data: ProgressIndicatorThemeData(
-            color: Theme.of(context).colorScheme.secondary,
-          ),
-          child: child,
-        ),
+        child: child,
       ),
     );
   }
